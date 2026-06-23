374dc4fbb856d2a8400fc5c9bcd60ddf310df47a
JohnVilla <129530641+johnathanvillalona@users.noreply.github.com>
2026-06-23 19:44:08 -0400

    Complete js exercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (num1===num2) return num1;
+    if (num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return "ERROR";
+    }
 
+    let rangeStart;
+    let rangeEnd;
+    let sum = 0;
+    if (num1>num2) {
+        rangeStart = num2;
+        rangeEnd = num1;
+    } else {
+        rangeStart = num1;
+        rangeEnd = num2;
+    }
+    
+    for (let i = rangeStart; i<=rangeEnd; i++) {sum += i};
+    return sum;
 };
 
 // Do not edit below this line
