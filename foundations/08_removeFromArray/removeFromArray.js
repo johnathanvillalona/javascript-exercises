02d2c0f10d06744ab97f1157065b82692f50a47e
JohnVilla <129530641+johnathanvillalona@users.noreply.github.com>
2026-06-23 19:20:34 -0400

    Complete js exercise

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...itemsToRemove) {
+    for (const item of itemsToRemove) {
+        while (array.includes(item)) {
+            array.splice(array.indexOf(item), 1)
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
